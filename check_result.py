934958016db2a8f69d3b68cbc8342d392c01a1b3
Григорий Петров <petrovgrigorij29@gmail.com>
2025-05-11 20:20:59 +0400

    Lab 2 added

--- check_result.py
+++ check_result.py
@@ -2,19 +2,18 @@ import numpy as np
 
 
 def check() -> None:
-    counts = [ 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1500, 2000, 3000]
+    counts = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1500, 2000, 3000]
     for i in counts:
-        matrix1 = np.loadtxt(f"1_{i}.txt", dtype=int)
-        matrix2 = np.loadtxt(f"2_{i}.txt", dtype=int)
+        matrix1 = np.loadtxt(f"data/1_{i}.txt", dtype=int)
+        matrix2 = np.loadtxt(f"data/2_{i}.txt", dtype=int)
         result = np.dot(matrix1, matrix2)
-        cpp_result = np.loadtxt(f"result_{i}.txt")
+        cpp_result = np.loadtxt(f"data/result_{i}.txt")
         if np.array_equal(cpp_result, result):
             print(
                 f"Проверка для матрицы {i} успешна: результаты совпадают"
             )
         else:
             print(" Ошибка: результаты не совпадают")
-
 
 
 if __name__ == "__main__":
--- graphic.py
+++ graphic.py
@@ -1,18 +1,31 @@
 import matplotlib.pyplot as plt
 
-with open('data/stats.txt', 'r') as file:
-    data = {}
-    for line in file:
-        size, time = line.strip().split(': ')
-        data[int(size)] = float(time)
 
-sizes = sorted(data.keys())
-times = [data[size] for size in sizes]
+def read_data(file_path):
+    data = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            size, time = line.strip().split(': ')
+            data[int(size)] = float(time)
+    return data
+
+
+data1 = read_data('data/stats_1.txt')
+data2 = read_data('data/stats_2.txt')
+
+sizes1 = sorted(data1.keys())
+times1 = [data1[size] for size in sizes1]
+sizes2 = sorted(data2.keys())
+times2 = [data2[size] for size in sizes2]
 
 plt.figure(figsize=(10, 6))
-plt.plot(sizes, times)
+plt.plot(sizes1, times1, label='Время', marker='o')
+plt.plot(sizes2, times2, label='Время с openmp', marker='x')
+
 plt.xlabel('Размер')
 plt.ylabel('Время, мс')
 plt.title('Зависимость времени от размера')
 plt.grid(True)
-plt.savefig('graphic_1.png')
+plt.legend()
+plt.savefig('graphic_comparison.png')
+plt.show()
